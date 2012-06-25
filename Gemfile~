cf0b192ba71c2867c9b5fe0815e8a79368c2ff3f
Carlos Eduardo <dudufangor@gmail.com>
2012-06-25 17:23:10 -0300

    first commit

---
+++ Gemfile~
@@ -0,0 +1,9 @@
+source :rubygems
+
+gem 'rails', '3.2.6'
+gem 'mysql2'
+
+group :development do
+	gem "pry", require: false
+	gem "awesome_print", require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerboard::Application.config.secret_token = 'a1a3a7838c2fbc52068c0dc1273815057c3308bbdb5fcadb42a00e664c32e48cfa092a48884e159490388292d3eeb3378c772cb748c1249d3d2bab759b7c1de7'
